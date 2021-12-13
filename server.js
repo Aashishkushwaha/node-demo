49fda2075bb1f1bff4d19d808d4dfd7239c6d0f4
Aashish Kushwaha <msdaashish@gmail.com>
2021-12-13 23:42:31 +0530

    node modules in nodejs

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-const Math = require("./math");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 
-console.log("Hello world!");
-// console.log(global);
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
 
-const os = require("os");
-const path = require("path");
-
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
-console.log(__dirname);
-console.log(__filename);
-
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
-
-console.log(path.parse(__filename));
-
-console.log(Math.add(2, 3));
-console.log(Math.subtract(2, 3));
-console.log(Math.multiply(2, 3));
-console.log(Math.divide(3, 2));
+console.log(uuid());
